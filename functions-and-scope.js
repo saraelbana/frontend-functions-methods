3c17ee657434e65c707d1f87a55fc88bf51095d1
saraelbana <matika20@gmail.com>
2024-01-18 00:15:09 +0200

    solving opdracht 1 in functions and scoope

--- functions-and-scope.js
+++ functions-and-scope.js
@@ -26,7 +26,17 @@ const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 // cumLaude([6, 4, 5]) geeft 0
 // cumLaude([8, 9, 4, 6, 10]) geeft 3
 
+function cumLaude(grades){
+    let totalCumLaude = 0;
+    for(let i =0; i<grades.length;i++)
+        if(grade[i]>=8)
+            totalCumLaude++;
 
+    return totalCumLaude;
+}
+console.log(cumLaude(grades));
+console.log(cumLaude([6, 4, 5]));
+console.log(cumLaude([8, 9, 4, 6, 10]));
 
 /* Opdracht  2: Gemiddeld cijfer */
 
